f735a1e4d2ccdc3bfdb12db8a78c4de644933db4
Alexandre Conte <alexandre.conte10@gmail.com>
2022-07-04 10:24:19 -0300

    Added front-end certification, updated staart links to certifications

--- src/components/Certifications/Certifications.jsx
+++ src/components/Certifications/Certifications.jsx
@@ -14,6 +14,7 @@ import startjs from "../../../public/images/certifications/startjs.jpg"
 import datastructure from "../../../public/images/certifications/data-structure.jpg"
 import web from "../../../public/images/certifications/web.jpg"
 import back from "../../../public/images/certifications/back.jpg"
+import front from "../../../public/images/certifications/frontend.jpg"
 
 import styles from "./Certifications.module.css"
 
@@ -32,10 +33,11 @@ const Certifications = () => {
         <CertificationItem href="https://www.dio.me/certificate/80508F95/share" src={oop} alt="JavaScript OOP" description="JavaScript Object Oriented Programming" />
         <CertificationItem href="https://www.udemy.com/certificate/UC-19010ef0-77d9-4873-ae6e-a1b4befa5bb6/" src={java} alt="Java: Programming logic certification" description="Java programming logic" />
         <CertificationItem href="https://proway.evolke.com.br/autoria/autoria/cert_imp.php?key=cb5f22472a905a9d322e1cd99e875076" src={jspro} alt="Advanced JavaScript certification" description="Advanced JavaScript" />
-        <CertificationItem href="https://app.staart.com/profile/certificates/f8dc79cc-ae72-4585-b629-9dc6b97dda8a" src={startjs} alt="First Lines of JavaScript code" description="JavaScript first lines" />
-        <CertificationItem href="https://app.staart.com/profile/certificates/edc54e0b-f451-43af-bff5-c61c3e8afdd8" src={datastructure} alt="JavaScript data structure" description="JavaScript Data Structures" />
-        <CertificationItem href="https://app.staart.com/profile/certificates/4937b101-35a3-4198-98e0-8b6378f58229" src={web} alt="Deconstructing the web" description="Deconstructing the web" />
-        <CertificationItem href="https://app.staart.com/profile/certificates/cb42842d-c0b8-4c55-a532-0b5dd3cd355f" src={back} alt="Back-End from scratch" description="Back-End from scratch" />
+        <CertificationItem href="https://assets.staart.com/app/_k/33f218a3-34e3-4257-9b3c-eab56572d2e5/323ae098-1624-4e24-8607-1f3a04718b11-496ce1bb-9dfc-48fa-91d7-db6bb375584b.pdf" src={startjs} alt="First Lines of JavaScript code" description="JavaScript first lines" />
+        <CertificationItem href="https://assets.staart.com/app/_k/33f218a3-34e3-4257-9b3c-eab56572d2e5/a69ee6be-b292-4cb9-a9a5-36481689a800-496ce1bb-9dfc-48fa-91d7-db6bb375584b.pdf" src={datastructure} alt="JavaScript data structure" description="JavaScript Data Structures" />
+        <CertificationItem href="https://assets.staart.com/app/_k/33f218a3-34e3-4257-9b3c-eab56572d2e5/468dbdd9-66bf-48ed-91fa-3dfdec5fba4e-496ce1bb-9dfc-48fa-91d7-db6bb375584b.pdf" src={web} alt="Deconstructing the web" description="Deconstructing the web" />
+        <CertificationItem href="https://assets.staart.com/app/_k/33f218a3-34e3-4257-9b3c-eab56572d2e5/23856860-f5b9-4b71-a92f-546902b8348c-496ce1bb-9dfc-48fa-91d7-db6bb375584b.pdf" src={back} alt="Back-End from scratch" description="Back-End from scratch" />
+        <CertificationItem href="https://assets.staart.com/app/_k/33f218a3-34e3-4257-9b3c-eab56572d2e5/19a31b7a-1286-4927-830e-564cbcd54a23-496ce1bb-9dfc-48fa-91d7-db6bb375584b.pdf" src={front} alt="Front-End from scratch" description="Front-End from scratch" />
       </ul>
     </div>
   )
