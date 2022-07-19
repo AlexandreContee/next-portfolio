61afc3d85ff439149ea4c673ad33d7a4a90d8ce0
Alexandre Conte <alexandre.conte10@gmail.com>
2022-07-18 21:37:36 -0300

    Added webtools certification

--- src/components/Certifications/Certifications.jsx
+++ src/components/Certifications/Certifications.jsx
@@ -19,6 +19,7 @@ import jsFundamentals from "../../../public/images/certifications/js-fundamental
 import vscode from "../../../public/images/certifications/vscode.jpg"
 import fsite from "../../../public/images/certifications/fsite.jpg"
 import git from "../../../public/images/certifications/git.jpg"
+import webtools from "../../../public/images/certifications/webtools.jpg"
 
 import styles from "./Certifications.module.css"
 
@@ -46,6 +47,7 @@ const Certifications = () => {
         <CertificationItem href="https://assets.staart.com/app/_k/33f218a3-34e3-4257-9b3c-eab56572d2e5/1a99deef-b707-4431-ae13-22a82c9b94eb-496ce1bb-9dfc-48fa-91d7-db6bb375584b.pdf" src={vscode} alt="vs-code environment" description="Visual Studio Code Environment" />
         <CertificationItem href="https://assets.staart.com/app/_k/33f218a3-34e3-4257-9b3c-eab56572d2e5/4ffd086c-868f-44d1-9f7f-3adf8d7edcc4-496ce1bb-9dfc-48fa-91d7-db6bb375584b.pdf" src={fsite} alt="Building the first HTML and CSS website" description="HTML and CSS" />
         <CertificationItem href="https://assets.staart.com/app/_k/33f218a3-34e3-4257-9b3c-eab56572d2e5/5286cb1e-96d7-4504-a66b-430262244dc8-496ce1bb-9dfc-48fa-91d7-db6bb375584b.pdf" src={git} alt="Git and GitHub" description="Git and Github" />
+        <CertificationItem href="https://assets.staart.com/app/_k/33f218a3-34e3-4257-9b3c-eab56572d2e5/ede24be4-c7f7-4190-b13a-1c178b4849c0-496ce1bb-9dfc-48fa-91d7-db6bb375584b.pdf" src={webtools} alt="Web tools" description="JavaScript web tools" />
       </ul>
     </div>
   )
